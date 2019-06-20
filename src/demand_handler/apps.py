5308c422e35189203349c625c47eebca01717ad1
abdelbasset-aidouni <ga_aidouni@esi.dz>
2019-06-20 03:15:13 +0100

    setup the backend and the db server

---
+++ src/demand_handler/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DemandHandlerConfig(AppConfig):
+    name = 'demand_handler'
---
+++ src/reservation_handler/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ReservationHandlerConfig(AppConfig):
+    name = 'reservation_handler'
